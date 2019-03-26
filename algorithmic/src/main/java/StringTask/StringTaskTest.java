de008692a4088069769ef4762f80d6821009968a
Aliahketich <mpoxom@gmail.com>
2019-03-26 15:41:01 +0300

    add start from unit-test

--- algorithmic/src/main/java/StringTask/StringTaskTest.java
+++ algorithmic/src/main/java/StringTask/StringTaskTest.java
@@ -7,9 +7,8 @@ import static StringTask.StringTask.transliteration;
 public class StringTaskTest {
 
     @Test
-    public void test(){
-        StringTask stringTask = new StringTask();
-        String text = "Азбука, ЕЖиК, ТеРеМок";
+    public void test() throws Exception {
+        String text = "Снегурочка, СНЕГОВИК, СНЕЖок, РоМаШкА";
         System.out.println(transliteration(text));
     }
 }
