6ca84b9cb9207faf80950969da72c53019f80262
Aliahketich <mpoxom@gmail.com>
2019-03-27 11:02:39 +0300

    work edition ArrayTask.

--- algorithmic/src/main/java/ArrayTask/ArrayTask.java
+++ algorithmic/src/main/java/ArrayTask/ArrayTask.java
@@ -44,6 +44,7 @@ public class ArrayTask {
                                     }
                                 }
                             }
+
                         }
                     }
                     deleteRowAndColumn(newArray);
