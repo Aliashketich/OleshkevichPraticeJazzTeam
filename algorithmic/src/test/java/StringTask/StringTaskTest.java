579603410cd974fb454679f2539a7fcbb356dbae
Aliahketich <mpoxom@gmail.com>
2019-03-28 15:57:53 +0300

    StringTaskTest changes

--- algorithmic/src/test/java/StringTask/StringTaskTest.java
+++ algorithmic/src/test/java/StringTask/StringTaskTest.java
@@ -3,11 +3,39 @@ package StringTask;
 import org.apache.log4j.Logger;
 import org.junit.*;
 
+import java.io.IOException;
+
 
 public class StringTaskTest {
 
     private static final Logger logger = Logger.getLogger(StringTaskTest.class);
-    StringTask stringTask = new StringTask();
+    StringTask stringTask;
+    String expectedString;
+
+    @Before
+    public void setUp() throws IOException {
+        stringTask = new StringTask();
+        expectedString = stringTask.transliteration();
+    }
+
+    @Test
+    public void runTransliterationTest() throws Exception {
+        logger.info("Имя теста: runTransliterationTest");
+        String actual = "Praktika eto prekrasno!";
+        Assert.assertEquals("Полученная строка не совпадает с тестовой", expectedString, actual);
+    }
+
+    @Test
+    public void testStringIsNull() throws Exception {
+        logger.info("Имя теста: testStringIsNull");
+        Assert.assertNotNull("Полученная строка null", expectedString);
+    }
+
+    @Ignore("Test has been ignored.")
+    @Test
+    public void ignoredTest() {
+        logger.info("will not print it");
+    }
 
     @BeforeClass
     public static void beforeClass() {
@@ -18,24 +46,4 @@ public class StringTaskTest {
     public static void afterClass() {
         logger.info("After StringTask.class");
     }
-
-    @Test
-    public void runTransliterationTest() throws Exception {
-        logger.info("Имя теста: runTransliterationTest");
-        String actual = "Praktika eto prekrasno!";
-        Assert.assertEquals("Полученная строка не совпадает с тестовой", stringTask.transliteration(), actual);
-    }
-
-    @Test
-    public void testStringIsNull() throws Exception {
-        logger.info("Имя теста: testStringIsNull");
-        Assert.assertNotNull("Полученная строка null", stringTask.transliteration());
-    }
-
-    @Ignore("Test has been ignored.")
-    @Test
-    public void ignoredTest() {
-        logger.info("will not print it");
-    }
-
 }
