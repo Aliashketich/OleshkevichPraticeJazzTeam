10f8bb3b1fc7d3ebca3c37042f61cc073b4867ce
Aliahketich <mpoxom@gmail.com>
2019-04-01 11:07:57 +0300

    HashMapTest Changes. set unicode for Jenkins builds.

--- algorithmic/src/test/java/Tasks/Collections/HashMapTask/HashMapTaskTest.java
+++ algorithmic/src/test/java/Tasks/Collections/HashMapTask/HashMapTaskTest.java
@@ -1,12 +1,13 @@
 package Tasks.Collections.HashMapTask;
 
-import exception.FileWorkException;
+import exception.MyExceptionForFileWork;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class HashMapTaskTest {
     private HashMapTask hashMapTask;
@@ -19,36 +20,38 @@ public class HashMapTaskTest {
     }
 
     @Test
-    public void checkResultOfTransliteration() throws IOException, FileWorkException {
+    public void checkResultOfTransliteration() throws IOException, MyExceptionForFileWork {
         String expectedString = "мы собираем конструктор";
         String vocabularyFilePath = "src/main/java/Tasks/Collections/HashMapTask/TextFileSources/vocabulary.txt";
-        Assert.assertEquals(expectedString, hashMapTask.translateStringToRussian(vocabularyFilePath, stringForTransliteration));
+        String actualString = new String(hashMapTask.translateStringToRussian(vocabularyFilePath, stringForTransliteration).getBytes(), StandardCharsets.UTF_8);
+        Assert.assertEquals(expectedString, actualString);
     }
 
     @Test(expected = FileNotFoundException.class)
-    public void vocabularyFileNotFoundExceptionProcessing() throws IOException, FileWorkException {
+    public void vocabularyFileNotFoundExceptionProcessing() throws IOException, MyExceptionForFileWork {
         String vocabularyFilePath = "src/main/java/Tasks/Collections/HashMapTask/TextFileSources/vocabularyNotExist.txt";
         hashMapTask.translateStringToRussian(vocabularyFilePath, stringForTransliteration);
     }
 
     @Test
-    public void someWordsIsNotInVocabularyTranslation() throws IOException, FileWorkException {
+    public void someWordsIsNotInVocabularyTranslation() throws IOException, MyExceptionForFileWork {
         String vocabularyFilePath = "src/main/java/Tasks/Collections/HashMapTask/TextFileSources/vocabulary.txt";
         String expectedString = "she собираем конструктор";
         String stringForTransliterationInThisTest = "she collect constructor";
-        Assert.assertEquals(expectedString, hashMapTask.translateStringToRussian(vocabularyFilePath, stringForTransliterationInThisTest));
+        String actualString = new String(hashMapTask.translateStringToRussian(vocabularyFilePath, stringForTransliterationInThisTest).getBytes(), StandardCharsets.UTF_8);
+        Assert.assertEquals(expectedString, actualString);
     }
 
     @Test
-    public void stringForTransliterationIsEmpty() throws IOException, FileWorkException {
+    public void stringForTransliterationIsEmpty() throws IOException, MyExceptionForFileWork {
         String vocabularyFilePath = "src/main/java/Tasks/Collections/HashMapTask/TextFileSources/vocabulary.txt";
         String expectedString = "";
         String stringForTransliterationInThisTest = "";
         Assert.assertEquals(expectedString, hashMapTask.translateStringToRussian(vocabularyFilePath, stringForTransliterationInThisTest));
     }
 
-    @Test(expected = FileWorkException.class)
-    public void vocabularyFileIsEmpty() throws IOException, FileWorkException {
+    @Test(expected = MyExceptionForFileWork.class)
+    public void vocabularyFileIsEmpty() throws IOException, MyExceptionForFileWork {
         String vocabularyFilePath = "src/main/java/Tasks/Collections/HashMapTask/TextFileSources/vocabularyIsEmpty.txt";
         Assert.assertEquals("Vocabulary file is empty!", hashMapTask.translateStringToRussian(vocabularyFilePath, stringForTransliteration));
     }
