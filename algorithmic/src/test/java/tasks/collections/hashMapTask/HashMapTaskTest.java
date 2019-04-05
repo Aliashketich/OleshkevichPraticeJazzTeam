ca4d4e01173c3f2bd4ce038d709316912c1ed984
Aliahketich <mpoxom@gmail.com>
2019-04-05 09:13:44 +0300

    hot-fixes HashMapTaskTest after Jenkins crashes

--- algorithmic/src/test/java/tasks/collections/hashMapTask/HashMapTaskTest.java
+++ algorithmic/src/test/java/tasks/collections/hashMapTask/HashMapTaskTest.java
@@ -8,8 +8,6 @@ import org.junit.Test;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import static util.strings.StringTaskUtils.saveInUnicode;
-
 public class HashMapTaskTest {
     private static final String ROOT_FILE_DIRECTORY_PATH = "src/main/resources/textFiles/tasks/collectionTaskTextFiles/hashMap/";
     private static final String VOCABULARY_PATH = "src/main/resources/textFiles/tasks/collectionTaskTextFiles/hashMap/vocabulary.txt";
@@ -21,10 +19,12 @@ public class HashMapTaskTest {
         hashMapTask = new HashMapTask();
         stringForTransliteration = "we collect constructor";
     }
+
     //упал тест на jenkins, после переноса в expected строки в русских символах
     @Test
     public void checkResultOfTranslationTest() throws IOException, MyException {
-        Assert.assertEquals(saveInUnicode("мы собираем конструктор"), hashMapTask.translateStringFromEnglishToRussian(VOCABULARY_PATH, stringForTransliteration));
+        String correctTranslateString = "мы собираем конструктор";
+        Assert.assertEquals(correctTranslateString, hashMapTask.translateStringFromEnglishToRussian(VOCABULARY_PATH, stringForTransliteration));
     }
 
     @Test(expected = FileNotFoundException.class)
@@ -37,7 +37,8 @@ public class HashMapTaskTest {
     @Test
     public void someWordsIsNotInVocabularyTranslationResultTest() throws IOException, MyException {
         String stringWithWordThatIsNotInVocabulary = "she collect constructor";
-        Assert.assertEquals(saveInUnicode("she собираем конструктор"), hashMapTask.translateStringFromEnglishToRussian(VOCABULARY_PATH, stringWithWordThatIsNotInVocabulary));
+        String correctTranslateString = "she собираем конструктор";
+        Assert.assertEquals(correctTranslateString, hashMapTask.translateStringFromEnglishToRussian(VOCABULARY_PATH, stringWithWordThatIsNotInVocabulary));
     }
 
     @Test
