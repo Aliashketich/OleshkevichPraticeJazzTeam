72f3bfa95d41fe7bb0d6ac8e0ca114efffac1818
Aliahketich <mpoxom@gmail.com>
2019-04-11 12:27:00 +0300

    jquery+ajax code style changes

--- web/src/web/jqueryajax/weather/weather.js
+++ web/src/web/jqueryajax/weather/weather.js
@@ -1,84 +1,40 @@
-let btnMinsk = document.getElementById("btnMinsk");
-btnMinsk.addEventListener('click', function () {
-    $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather",
-        data: {
-            "q": btnMinsk.getAttribute("value"),
-            "appid": "70e1ed322b02acbc57d443dd91065f3e"
-        },
-        success: function (data) {
-            parseAnswer(data);
-        }
-    });
+document.getElementById("btnLida").addEventListener('click', function () {
+    getData(document.getElementById("btnLida"));
+});
+document.getElementById("btnSoligorsk").addEventListener('click', function () {
+    getData(document.getElementById("btnSoligorsk"));
+});
+document.getElementById("btnMinsk").addEventListener('click', function () {
+    getData(document.getElementById("btnMinsk"));
+});
+document.getElementById("btnBrest").addEventListener('click', function () {
+    getData(document.getElementById("btnBrest"));
+});
+document.getElementById("btnKiev").addEventListener('click', function () {
+    getData(document.getElementById("btnKiev"));
+});
+document.getElementById("btnMoscow").addEventListener('click', function () {
+    getData(document.getElementById("btnMoscow"));
+});
+document.getElementById("btnBerlin").addEventListener('click', function () {
+    getData(document.getElementById("btnBerlin"));
+});
+document.getElementById("btnStolin").addEventListener('click', function () {
+    getData(document.getElementById("btnStolin"));
 });
 
-let btnBrest = document.getElementById("btnBrest");
-btnBrest.addEventListener('click', function () {
+function getData(btn) {
     $.ajax({
         url: "http://api.openweathermap.org/data/2.5/weather",
         data: {
-            "q": btnBrest.getAttribute("value"),
+            "q": btn.getAttribute("value"),
             "appid": "70e1ed322b02acbc57d443dd91065f3e"
         },
         success: function (data) {
             parseAnswer(data);
         }
     });
-});
-
-let btnSoligorsk = document.getElementById("btnSoligorsk");
-btnSoligorsk.addEventListener('click', function () {
-    $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather",
-        data: {
-            "q": btnSoligorsk.getAttribute("value"),
-            "appid": "70e1ed322b02acbc57d443dd91065f3e"
-        },
-        success: function (data) {
-            parseAnswer(data);
-        }
-    });
-});
-
-let btnMoscow = document.getElementById("btnMoscow");
-btnMoscow.addEventListener('click', function () {
-    $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather",
-        data: {
-            "q": btnMoscow.getAttribute("value"),
-            "appid": "70e1ed322b02acbc57d443dd91065f3e"
-        },
-        success: function (data) {
-            parseAnswer(data);
-        }
-    });
-});
-let btnBerlin = document.getElementById("btnBerlin");
-btnBerlin.addEventListener('click', function () {
-    $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather",
-        data: {
-            "q": btnBerlin.getAttribute("value"),
-            "appid": "70e1ed322b02acbc57d443dd91065f3e"
-        },
-        success: function (data) {
-            parseAnswer(data);
-        }
-    });
-});
-let btnLida = document.getElementById("btnLida");
-btnLida.addEventListener('click', function () {
-    $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather",
-        data: {
-            "q": btnLida.getAttribute("value"),
-            "appid": "70e1ed322b02acbc57d443dd91065f3e"
-        },
-        success: function (data) {
-            parseAnswer(data);
-        }
-    });
-});
+}
 
 function parseAnswer(data) {
     let out = '';
